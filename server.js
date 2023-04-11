e1c8cecf709d2a2ee3c48287c3e657a8d079a90e
mmubo <107107672+mmubo@users.noreply.github.com>
2023-04-11 16:56:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const username = process.env.WEB_USERNAME || "admin";
 const password = process.env.WEB_PASSWORD || "password";
-const hrl = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
@@ -16,59 +15,61 @@ app.get("/", function (req, res) {
   res.send("hello world");
 });
 
+// 页面访问密码
 app.use((req, res, next) => {
   const user = auth(req);
   if (user && user.name === username && user.pass === password) {
     return next();
   }
-  res.set('WWW-Authenticate', 'Basic realm="Node"');
+  res.set("WWW-Authenticate", 'Basic realm="Node"');
   return res.status(401).send();
 });
 
-// 获取系统进程表
+//获取系统进程表
 app.get("/status", function (req, res) {
-  let cmdStr =
-    "ps -ef";
+  let cmdStr = "pm2 list; ps -ef";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
     } else {
-      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+      res.type("html").send("<pre>获取守护进程和系统进程表：\n" + stdout + "</pre>");
     }
   });
 });
 
-// 获取系统监听端口
+//获取系统监听端口
 app.get("/listen", function (req, res) {
-  let cmdStr = "ss -nltp";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    } else {
-      res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
-    }
+    let cmdStr = "ss -nltp";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      } else {
+        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+      }
+    });
   });
-});
 
 //获取节点数据
 app.get("/list", function (req, res) {
-  let cmdStr = "cat list";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    } else {
-      res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
-    }
+    let cmdStr = "bash argo.sh";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+      }
+    });
   });
-});
 
-// 获取系统版本、内存信息
+//获取系统版本、内存信息
 app.get("/info", function (req, res) {
   let cmdStr = "cat /etc/*release | grep -E ^NAME";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("命令行执行错误：" + err);
-    } else {
+    }
+    else {
       res.send(
         "命令行执行结果：\n" +
           "Linux System:" +
@@ -81,92 +82,32 @@ app.get("/info", function (req, res) {
   });
 });
 
-// 文件系统只读测试
+//文件系统只读测试
 app.get("/test", function (req, res) {
   fs.writeFile("./test.txt", "这里是新创建的文件内容!", function (err) {
     if (err) {
       res.send("创建文件失败，文件系统权限为只读：" + err);
-    } else {
+    }
+    else {
       res.send("创建文件成功，文件系统权限为非只读：");
     }
   });
 });
 
-// 启动root
-app.get("/root", function (req, res) {
-  let cmdStr = "bash root.sh >/dev/null 2>&1 &";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.send("root权限部署错误：" + err);
-    } else {
-      res.send("root权限执行结果：" + "启动成功!");
-    }
-  });
-});
-
+// keepalive begin
 //web保活
 function keep_web_alive() {
-  // 1.请求主页，保持唤醒
-  exec("curl -m5 " + hrl, function (err, stdout, stderr) {
+  // 请求主页，保持唤醒
+  exec("curl -m8 127.0.0.1:" + port, function (err, stdout, stderr) {
     if (err) {
       console.log("保活-请求主页-命令行执行错误：" + err);
-    } else {
-      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
     }
-  });
-  // 2.请求服务器进程状态列表，若web没在运行，则调起
-  exec("pgrep -laf starlink", function (err, stdout, stderr) {
-    // 1.查后台系统进程，保持唤醒
-    if (stdout.includes("./hi")) {
-      console.log("hi 正在运行");
-    } else {
-      //web 未运行，命令行调起
-      exec(
-        "chmod +x ./hi && nohup ./hi 2>&1 &",
-        function (err, stdout, stderr) {
-          if (err) {
-            console.log("保活-调起hi-命令行执行错误:" + err);
-          } else {
-            console.log("保活-调起hi-命令行执行成功!");
-          }
-        }
-      );
+    else {
+      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
     }
   });
 }
 setInterval(keep_web_alive, 10 * 1000);
-
-//Argo保活
-function keep_argo_alive() {
-  exec("pgrep -laf cloudflared", function (err, stdout, stderr) {
-    // 1.查后台系统进程，保持唤醒
-    if (stdout.includes("./cloudflared tunnel")) {
-      console.log("Argo 正在运行");
-    } else {
-      //Argo 未运行，命令行调起
-      exec("bash argo.sh 2>&1 &", function (err, stdout, stderr) {
-        if (err) {
-          console.log("保活-调起Argo-命令行执行错误:" + err);
-        } else {
-          console.log("保活-调起Argo-命令行执行成功!");
-        }
-      });
-    }
-  });
-}
-setInterval(keep_argo_alive, 30 * 1000);
-// keepalive end
-
-//下载helloworld可执行文件
-app.get("/download", function (req, res) {
-  download_web((err) => {
-    if (err) {
-      res.send("下载文件失败");
-    } else {
-      res.send("下载文件成功");
-    }
-  });
-});
 
 app.use(
   "/",
@@ -175,42 +116,14 @@ app.use(
     onProxyReq: function onProxyReq(proxyReq, req, res) {},
     pathRewrite: {
       // 请求中去除/
-      "^/": "/",
+      "^/": "/"
     },
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
-    ws: true, // 是否代理websockets
+    ws: true // 是否代理websockets
   })
 );
 
-
-
-//初始化，下载web
-function download_web(callback) {
-  let fileName = "hi";
-  let web_url =
-    "https://github.com/mmubo/sky/releases/download/sky/hi";
-  let stream = fs.createWriteStream(path.join("./", fileName));
-  request(web_url)
-    .pipe(stream)
-    .on("close", function (err) {
-      if (err) {
-        callback("下载文件失败");
-      } else {
-        callback(null);
-      }
-    });
-}
-
-download_web((err) => {
-  if (err) {
-    console.log("初始化-下载hi文件失败");
-  } else {
-    console.log("初始化-下载hi文件成功");
-  }
-});
-
-
-// 启动核心脚本运行web,哪吒和argo
+//启动核心脚本运行web,哪吒和argo
 exec("bash entrypoint.sh", function (err, stdout, stderr) {
   if (err) {
     console.error(err);
